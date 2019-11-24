eabc461a4f7a29ce6ce0c488de499b260396ded2
Paul M. Rodriguez <pmr@ruricolist.com>
2019-11-24 13:08:51 -0600

    Remove test suite on Lisp side.

--- t/test.lisp
+++ t/test.lisp
@@ -5,26 +5,5 @@
 (in-package :cloture.test)
 (in-readtable clojure-shortcut)
 
-(def-suite cloture)
-(in-suite cloture)
-
 (defun run-cloture-tests ()
-  (run! 'cloture)
   (|clojure.test|:|run-tests| (find-package "cloture.tests")))
-
-(defmacro is-truthy? (condition &rest reason-args)
-  `(if (truthy? ,condition)
-       (5am::add-result '5am::test-passed :test-expr ',condition)
-       (5am::process-failure ',condition
-                             ,@(or reason-args
-                                   `("~S did not return a truthy value" ',condition)))))
-
-(defmacro is-falsy? (condition &rest reason-args)
-  (with-gensyms (value)
-    `(let ((,value ,condition))
-       (if (truthy? ,value)
-           (5am::process-failure ',condition
-                                 ,@(or reason-args
-                                       `("~S returned the value ~S, which Clojure considers truthy" ',condition ,value)))
-           (5am::add-result '5am::test-passed :test-expr ',condition)))))
-
