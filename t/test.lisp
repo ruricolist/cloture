76594b524e944dd693079ba9af3d11c891e59230
Paul M. Rodriguez <pmr@ruricolist.com>
2019-11-28 18:15:03 -0600

    Add cl-travis.

--- t/test.lisp
+++ t/test.lisp
@@ -1,7 +1,8 @@
 (defpackage :cloture.test
   (:use :cl :alexandria :serapeum :fiveam :cloture :named-readtables)
   (:import-from :fset :equal? :seq :convert)
-  (:shadowing-import-from :fset :map :set))
+  (:shadowing-import-from :fset :map :set)
+  (:export :run-cloture-tests))
 (in-package :cloture.test)
 (in-readtable clojure-shortcut)
 
