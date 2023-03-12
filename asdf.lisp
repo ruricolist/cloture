f9a91752ad5dbbc658be3f09489692a6f6307435
Paul M. Rodriguez <pmr@ruricolist.com>
2023-03-12 12:17:18 -0500

    Work around ASDF package change

--- asdf.lisp
+++ asdf.lisp
@@ -9,7 +9,12 @@
 (defclass cljs (clj)
   ((type :initform "cljs")))
 
+(defmethod asdf/lisp-action:call-with-around-compile-hook ((c clj) thunk)
+  (let ((*package* (find-package "user")))
+    (call-next-method)))
+
 (defmethod asdf:perform :around ((o asdf:compile-op) (c clj))
+  ;; This doesn't work; ASDF forcibly rebinds to cl-user.
   (let ((*package* (find-package "user")))
     (with-clojure-reader ()
       (call-next-method))))
