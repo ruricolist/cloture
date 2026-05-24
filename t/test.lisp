11358e57542dcd8eef6f3c166784c516051084ef
Paul M. Rodriguez <pmr@ruricolist.com>
2026-05-23 20:55:26 -0500

    Provide an entry point for running individual tests by name

--- t/test.lisp
+++ t/test.lisp
@@ -2,7 +2,7 @@
   (:use :cl :alexandria :serapeum :fiveam :cloture :named-readtables)
   (:import-from :fset :equal? :seq :convert)
   (:shadowing-import-from :fset :map :set)
-  (:export :run-cloture-tests))
+  (:export :run-cloture-tests :run-cloture-test))
 (in-package :cloture.test)
 (in-readtable clojure-shortcut)
 
@@ -12,3 +12,9 @@
     (when (and (zerop (fset:lookup result :|pass|))
                (zerop (fset:lookup result :|fail|)))
       (error "No tests run"))))
+
+(defun run-cloture-test (test-name)
+  (let ((cl-sym
+          (or (find-symbol (string test-name) "cloture.tests")
+              (error "No such test: ~a" test-name))))
+    (|clojure.test|::|test-var| (|clojure.core|:|find-var| cl-sym))))
